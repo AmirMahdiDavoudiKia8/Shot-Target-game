3f53a107b2e0f9d9ad80b9191a6ff502e78913bf
sinanp <sina.poco.77@gmail.com>
2025-03-07 13:12:43 +0330

    add class player

--- project-class.py
+++ project-class.py
@@ -1,6 +1,65 @@
-import pygame
+import pygame,sys
+import pygame.event as GAME_EVENTS
+import pygame.locals as GAME_GLOBALS
 import random
 
+
+pygame.init()
+window=pygame.display.set_mode((900, 600))
+window.fill((10, 18, 18))
+pygame.display.set_caption('Shot-Target')
+
+while True:
+    for event in GAME_EVENTS.get():
+        if event.type == GAME_GLOBALS.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+
+class pleyer:
+
+    def __init__(self, name, score=0, time=300, Ammo=20):
+        self.name = name
+        self.score = score
+        self.time = time
+        self.Ammo = Ammo
+        self.pos_x = random.randiat(1, 90) * 10
+        self.pos_y = random.randiat(1, 60) * 10
+
+    def move_up(self):
+        self.pos_y += 10
+        if self.pos_y > 600 :
+            self.pos_y - 600
+
+    def move_done(self):
+        self.pos_y -= 10
+        if self.pos_y < 0 :
+            self.pos_y + 600
+
+    def move_right(self):
+        self.pos_x += 10
+        if self.pos_x > 900 :
+            self.pos_x -= 900
+
+    def move_left(self):
+        self.pos_x -= 10
+        if self.pos_x < 0 :
+            self.pos_x += 900
+
+
+    def shoot(self, player):
+        if self.Ammo > 0 :
+            self.Ammo -= 1
+
+
+
+    def Hit(self, target):
+        if target.x - 20 < self.pos_x < target.x + 20 and target.y - 20 < self.pos_y < target.y + 20 :
+            target.rest()
+
+            
 class Target:
     def __init__(self):
         self.x = random.randit(0, 88) * 10
