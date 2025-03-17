73df933d56685ae1ec59112058fe3b0333ded420
sinanp <sina.poco.77@gmail.com>
2025-03-17 13:11:39 +0330

    Time management, shot management, game ending, and debugging

--- project-class.py
+++ project-class.py
@@ -7,6 +7,97 @@ pygame.init()
 pygame.mixer.init()
 shoot_sound = pygame.mixer.Sound("shoot.wav")
 
+                     
+class Player:
+
+    def __init__(self, name, score=0, time=180, Ammo=20):
+        self.name = name
+        self.score = score
+        self.time = time
+        self.Ammo = Ammo
+        self.pos_x = random.randint(1, 90) * 10
+        self.pos_y = random.randint(8, 68) * 10
+        self.last_shot_pos = None 
+        self.score_system = ScoreSystem()
+        self.timer_started = False
+
+    def move_up(self):
+        self.pos_y -= 10
+        if self.pos_y <= 80:
+            self.pos_y += 600
+
+    def move_down(self):
+        self.pos_y += 10
+        if self.pos_y >= 680:
+            self.pos_y -= 600
+
+
+    def move_right(self):
+        self.pos_x += 10
+        if self.pos_x > 900 :
+            self.pos_x -= 900
+
+    def move_left(self):
+        self.pos_x -= 10
+        if self.pos_x < 0 :
+            self.pos_x += 900
+
+
+    def shoot(self, player, targets, screen):
+        if self.Ammo > 0:
+            self.Ammo -= 1
+            self.last_shot_pos = (self.pos_x, self.pos_y)
+            shoot_sound.play()
+            if not self.timer_started:
+                self.timer_started = True
+            self.show_Crosshair(screen)
+            self.hit(player, targets)
+
+
+    def show_Crosshair(self, screen):
+    
+        if self.last_shot_pos:
+            x, y = self.last_shot_pos
+            pygame.draw.rect(screen, (255, 0, 0), (x - 5, y - 5, 10, 10))
+            pygame.draw.circle(screen, (0, 0, 0), (x, y), 3)
+
+
+    def hit(self, opponent, targets):
+        if not self.last_shot_pos:
+            return False
+
+        shot_x, shot_y = self.last_shot_pos 
+    
+        for t in targets[:]:
+
+            if t.x  < shot_x <= t.x + 40  and t.y  <= shot_y < t.y + 40:
+                print(f"Hit target at {t.x}, {t.y}")
+                targets.remove(t)
+
+                if isinstance(t, ExtraTime):
+                    self.time += 30
+                    targets.append(ExtraTime())
+                    self.score_system.reset_consecutive()
+
+                elif isinstance(t, ExtraAmmo):
+                    self.Ammo += 5
+                    targets.append(ExtraAmmo())
+                    self.score_system.reset_consecutive()
+
+                elif isinstance(t, loseAmmo):
+                    if opponent.Ammo > 0:
+                        opponent.Ammo -= 2
+                    targets.append(loseAmmo())
+                    self.score_system.reset_consecutive()
+
+                else:
+                    score = self.score_system.calculate_score(self.pos_x, self.pos_y, t.x, t.y)
+                    self.score += score
+                    targets.append(Target())
+                return True
+        self.score_system.reset_consecutive()
+
+
 class ScoreSystem:
     def __init__(self):
         self.consecutive_hits = 0
@@ -32,89 +123,13 @@ class ScoreSystem:
     
     def reset_consecutive(self):
         self.consecutive_hits = 0
-                     
-class Player:
-
-    def __init__(self, name, score=0, time=60, Ammo=20):
-        self.name = name
-        self.score = score
-        self.time = time
-        self.Ammo = Ammo
-        self.pos_x = random.randint(1, 90) * 10
-        self.pos_y = random.randint(1, 60) * 10
-        self.last_shot_pos = None 
-        self.score_system = ScoreSystem()
-
-    def move_up(self):
-        self.pos_y -= 10
-        if self.pos_y < 80:
-            self.pos_y = 600
-
-    def move_down(self):
-        self.pos_y += 10
-        if self.pos_y > 600:
-            self.pos_y = 80
 
 
-    def move_right(self):
-        self.pos_x += 10
-        if self.pos_x > 900 :
-            self.pos_x -= 900
-
-    def move_left(self):
-        self.pos_x -= 10
-        if self.pos_x < 0 :
-            self.pos_x += 900
-
-
-    def shoot(self, player, targets, screen):
-        if self.Ammo > 0:
-            self.Ammo -= 1
-            self.last_shot_pos = (self.pos_x, self.pos_y)
-            shoot_sound.play()
-            self.show_Crosshair(screen)
-            self.hit(player, targets)
-
-    def show_Crosshair(self, screen):
-    
-        if self.last_shot_pos:
-            x, y = self.last_shot_pos
-            pygame.draw.rect(screen, (255, 0, 0), (x - 5, y - 5, 10, 10))
-            pygame.draw.circle(screen, (0, 0, 0), (x, y), 3)
-
-
-    def hit(self, opponent, targets):
-        for t in targets[:]:
-            if t.x - 20 < self.pos_x < t.x + 20 and t.y - 20 < self.pos_y < t.y + 20:
-                print(f"Hit target at {t.x}, {t.y}") 
-                targets.remove(t)
-                if isinstance(t, ExtraTime):
-                    self.time += 10
-                    targets.append(ExtraTime())
-                    self.score_system.reset_consecutive()
-
-                elif isinstance(t, ExtraAmmo):
-                    self.Ammo += 5
-                    targets.append(ExtraAmmo())
-                    self.score_system.reset_consecutive()
-
-                elif isinstance(t, loseAmmo):
-                    opponent.Ammo -= 2
-                    targets.append(loseAmmo())
-                    self.score_system.reset_consecutive()
-
-                else:
-                    score = self.score_system.calculate_score(self.pos_x, self.pos_y, t.x, t.y)
-                    self.score += score
-                    targets.append(Target())
-                return True
-        self.score_system.reset_consecutive()
-            
             
 class Target:
     def __init__(self):
-        self.x = random.randint(0, 88) * 10
-        self.y = random.randint(10, 58) * 10
+        self.x = random.randint(0, 86) * 10
+        self.y = random.randint(12, 64) * 10
         self.score = 10
         self.image = pygame.image.load("target.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 40))
@@ -135,9 +150,10 @@ class ExtraAmmo(Target):
 
 class loseAmmo(Target):
     def __init__(self):
+        super().__init__()
         self.image = pygame.image.load("decrease.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 40))
-        super().__init__()
+
         
     def show(self, screen):
         screen.blit(self.image, (self.x, self.y))
@@ -256,6 +272,8 @@ Targets = [Target1, Target2, Target3, item1, item2, item3]
 num_shoot1 = 0
 num_shoot2 = 0
 
+last_time_update = pygame.time.get_ticks()
+
 running = True
 while running:
     screen.fill(GRAY)
@@ -272,6 +290,26 @@ while running:
 
     
     pygame.display.flip()
+
+    current_time = pygame.time.get_ticks()
+    if current_time - last_time_update >= 1000:
+        last_time_update = current_time
+
+        if player1.timer_started and player1.time > 0:
+            player1.time -= 1
+
+        if player2.timer_started and player2.time > 0:
+            player2.time -= 1
+
+
+    if player1.time <= 0 and player2.time <= 0:
+        running = False
+        break
+
+    if player1.Ammo <= 0 and player2.Ammo <= 0:
+        running = False
+        break
+
     
     for event in pygame.event.get():
         print(event)
@@ -281,12 +319,13 @@ while running:
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_e:
-                player1.shoot(player2, Targets, screen)
-                num_shoot1 += 1
-                for target in Targets:
-                    target.show(screen)
+                if player1.time > 0:
+                    player1.shoot(player2, Targets, screen)
+                    num_shoot1 += 1
+                    for target in Targets:
+                        target.show(screen)
 
-                    pygame.display.flip()
+                        pygame.display.flip()
 
 
             if event.key == pygame.K_w:
@@ -303,12 +342,13 @@ while running:
 
 
             if event.key == pygame.K_KP7:
-                player2.shoot(player1, Targets, screen)
-                num_shoot2 += 1
-                for target in Targets:
-                    target.show(screen)
+                if player2.time > 0:
+                    player2.shoot(player1, Targets, screen)
+                    num_shoot2 += 1
+                    for target in Targets:
+                        target.show(screen)
 
-                    pygame.display.flip()
+                        pygame.display.flip()
 
 
             if event.key == pygame.K_KP8:
